40610fab4f3ac349207f8b2734bb4550438cbd5e
ChristianAlexa <cjalexa@gmail.com>
2021-04-08 23:52:47 -0400

    3 by 3 proof of concept

--- src/aji/db.cljs
+++ src/aji/db.cljs
@@ -1,4 +1,13 @@
 (ns aji.db)
 
 (def default-db
-  {:name "aji"})
+  {:name "aji"
+   :board {"1a" {:stone nil :liberties 4}
+           "2a" {:stone nil :liberties 4}
+           "3a" {:stone nil :liberties 4}
+           "1b" {:stone nil :liberties 4}
+           "2b" {:stone nil :liberties 4}
+           "3b" {:stone nil :liberties 4}
+           "1c" {:stone nil :liberties 4}
+           "2c" {:stone nil :liberties 4}
+           "3c" {:stone nil :liberties 4}}})
--- src/board/board.cljs
+++ src/board/board.cljs
@@ -1,6 +1,56 @@
-(ns board.board)
+(ns board.board
+  (:require [re-frame.core :as rf]))
+
+;; -----------------------------------------------------------------------------
+;; EVENT HANDLERS
+;; (rf/reg-event-db
+;;  ::init-board
+;;  (fn db [_ _]))
+
+;; -----------------------------------------------------------------------------
+;; SUBSCRIPTIONS
+(rf/reg-sub
+ ::get-starting-board
+ (fn [db [_ _]]
+   (:board db)))
+
+;; -----------------------------------------------------------------------------
+;; VIEWS
+
+(defn Intersection
+  [coord]
+  (let [last-intersection (second coord)]
+    [:<>
+     [:li  {:style {:border "1px solid black"
+                    :backgroundColor "#F9D17C"
+                    :height "100px"
+                    :width "100px"
+                    :text-align "center"
+                    :display "inline-block"}} coord]
+     (when (= "c" last-intersection)
+       [:br])]))
+
+(defn build-all-rows
+  [initial-board]
+  (let [row1 (into [] (filter #(= "1" (first %)) (keys initial-board)))
+        row2 (into [] (filter #(= "2" (first %)) (keys initial-board)))
+        row3 (into [] (filter #(= "3" (first %)) (keys initial-board)))
+        all-rows []]
+    (-> all-rows
+        (conj  (into [] (sort (select-keys initial-board row1))))
+        (conj  (into [] (sort (select-keys initial-board row2))))
+        (conj  (into [] (sort (select-keys initial-board row3)))))))
+
+(defn display-row
+  [row]
+  (map (fn [row] [Intersection (first row)]) row))
 
 (defn Board
   "Board displays the game board."
   []
-  [:h1 "Go board"])
+  (let [initial-board @(rf/subscribe [::get-starting-board])
+        all-rows (build-all-rows initial-board)]
+    [:div {:id "boardContainer"}
+     [:h1 "Go board"]
+     [:ul
+      (map (fn [row] (display-row row)) all-rows)]]))
