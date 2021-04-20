b12853b5c42d1590072ce4cf4d32eeb1500e0db5
Christian Alexa <Cjalexa@gmail.com>
2021-04-19 23:42:14 -0400

    Add puzzle pattern 1 (#12)

---
+++ src/aji/components/capture_panel.cljs
@@ -0,0 +1,25 @@
+(ns aji.components.capture-panel
+  (:require [re-frame.core :as rf]))
+
+;; -----------------------------------------------------------------------------
+;; SUBSCRIPTIONS
+(rf/reg-sub
+ ::num-white-captured-stones
+ (fn [db [_ _]]
+   (get-in db [:active-board :white-captured-stones])))
+
+(rf/reg-sub
+ ::num-black-captured-stones
+ (fn [db [_ _]]
+   (get-in db [:active-board :black-captured-stones])))
+
+;; -----------------------------------------------------------------------------
+;; VIEWS
+(defn CapturePanel
+  []
+  (let [white-captured-stones @(rf/subscribe [::num-white-captured-stones])
+        black-captured-stones @(rf/subscribe [::num-black-captured-stones])]
+    [:div.panel.is-info {:id "capturePanel"}
+     [:p.panel-heading "Captures"]
+     [:div.panel-block "White stones captured: " white-captured-stones]
+     [:div.panel-block "Black stones captured: " black-captured-stones]]))
---
+++ src/aji/puzzles/patterns.cljs
@@ -0,0 +1,21 @@
+(ns aji.puzzles.patterns)
+
+(def PATTERNS
+  {"PUZZLE_PATTERN_1"
+
+   {"2-C" {:stone "BLACK"
+           :liberties 3
+           :position "POSITION_MIDDLE"
+           :neighbors nil}
+    "3-D" {:stone "BLACK"
+           :liberties 3
+           :position "POSITION_MIDDLE"
+           :neighbors nil}
+    "4-C" {:stone "BLACK"
+           :liberties 3
+           :position "POSITION_MIDDLE"
+           :neighbors nil}
+    "3-C" {:stone "WHITE"
+           :liberties 1
+           :position "POSITION_MIDDLE"
+           :neighbors nil}}})
